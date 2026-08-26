6336a3d07e202c4af9792ad104b21a2d33bba4cc
Wajza <s202262140@kfupm.edu.sa>
2026-08-26 21:53:04 +0300

    Checkpoint 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,38 @@
-// Checkpoint 2
+// Checkpoint 3
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Swe 463 - Demo 1'),
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
+        ),
+        drawer: Drawer(
+          child: SafeArea(
+            child: Column(
+              children: [
+                DrawerHeader(
+                  child: Center(
+                    child: const Text(
+                      'Demo Menu',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ), // Header closed properly here
+                const ListTile(title: Text('Home'), leading: Icon(Icons.home)),
+              ],
+            ),
+          ),
+        ),
         body: Center(
-          child: Text('Welcome to swe 463!', style: TextStyle(fontSize: 24)),
+          child: const Text(
+            'Open the drawer from the menu icon',
+            style: TextStyle(fontSize: 20),
+          ),
         ),
       ),
     ),
