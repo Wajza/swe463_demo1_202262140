a81af9cd747e4112a8461cbaaffe507f264f5362
Wajza <s202262140@kfupm.edu.sa>
2026-08-26 21:31:34 +0300

    Checkpoint 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
-// Checkpoint 1
+// Checkpoint 2
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text("My name is Wajd Alghamdi")));
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Welcome to swe 463!', style: TextStyle(fontSize: 24)),
+        ),
+      ),
+    ),
+  );
 }
