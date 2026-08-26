d4416af03f0e72410d34c00e5050f485ddaa9166
Wajza <s202262140@kfupm.edu.sa>
2026-08-26 22:24:30 +0300

    Checkpoint 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// Checkpoint 4
+// Checkpoint 5
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,16 +12,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('SWE 463 - Demo 1')),
-        drawer: const Drawer(
-          child: SafeArea(
-            child: ListTile(leading: Icon(Icons.home), title: Text('Home')),
-          ),
-        ),
-        body: const Center(
-          child: Text('Hot reload is working!', style: TextStyle(fontSize: 24)),
-        ),
+      title: 'SWE 463 Demo 1',
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: const Center(
+        child: Text('This is the Home Page', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login Page')),
+      body: const Center(
+        child: Text('This is the Login Page', style: TextStyle(fontSize: 24)),
       ),
     );
   }
