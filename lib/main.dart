6d816640907674ce1d95a0a15012209b6776fc13
Wajza <s202262140@kfupm.edu.sa>
2026-08-26 22:37:55 +0300

    Checkpoint 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,43 @@
-// Checkpoint 5
+// Checkpoint 6
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'SWE 463 Demo 1',
-      home: const HomePage(),
-    );
-  }
+  runApp(MaterialApp(home: HomePage()));
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
-      body: const Center(
-        child: Text('This is the Home Page', style: TextStyle(fontSize: 24)),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to Login'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LoginPage()),
+            );
+          },
+        ),
       ),
     );
   }
 }
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login Page')),
-      body: const Center(
-        child: Text('This is the Login Page', style: TextStyle(fontSize: 24)),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to Home'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+        ),
       ),
     );
   }
